6a95fd122bcbd7d8e1bc87ef82b243d4110c94ce
Sergey Nam <shaqk702@gmail.com>
2022-01-09 20:14:38 -0800

    Add button message

---
+++ pages/song/messages.ts
@@ -0,0 +1,1 @@
+export const MSG_BUTTON = "Detect Song"
--- pages/song/song.tsx
+++ pages/song/song.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Button } from '../../components'
 import styles from '../../styles/Home.module.scss'
 
+import { MSG_BUTTON } from './messages'
+
 export const Song = () => {
   return (
     <div className={styles.main}>
-      <Button variant="contained">Click Me</Button>
+      <Button variant="contained">{MSG_BUTTON}</Button>
   </div>
   )
 }
