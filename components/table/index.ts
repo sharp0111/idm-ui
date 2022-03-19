993d72c0a6548fbc98e5cb342a90f6dd4c6cc436
Sergey Nam <shaqk702@gmail.com>
2022-03-18 18:09:27 -0700

    Add Typography to components

--- components/table/index.ts
+++ components/table/index.ts
@@ -1,1 +1,1 @@
-export { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material' 
+export { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@mui/material'
