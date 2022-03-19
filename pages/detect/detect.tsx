85c1a3de8c39219395226104fa995a4cbdb52d5b
Sergey Nam <shaqk702@gmail.com>
2022-03-18 18:24:33 -0700

    Remove unnecessary component

--- pages/detect/detect.tsx
+++ pages/detect/detect.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Button } from '../../components'
 import styles from './detect.module.scss'
-
-import { MSG_BUTTON } from './messages'
 
 export const Detect = () => {
   return (
     <div className={styles.main}>
-      <Button color="secondary" variant="contained" size="large">{MSG_BUTTON}</Button>
+      Detect Page
   </div>
   )
 }
