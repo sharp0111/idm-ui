e5599b2fba36ac36570a2ccc3020d37fbc7c02c8
Sergey Nam <shaqk702@gmail.com>
2022-04-03 11:40:45 -0700

    Update home page

--- pages/home/home-container.tsx
+++ pages/home/home-container.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import { Button } from '../../components'
-import { ActiveLink, IDM_URL } from '../routing'
 
-import { MSG_IDM } from './messages'
+import Idm from '../idm'
 
 export function HomeContainer() {
   return (
-    <>
-      <ActiveLink href={IDM_URL}>
-        <Button color="secondary" variant="contained" size="large">{MSG_IDM}</Button>
-      </ActiveLink>
-    </>
+    <Idm />
   )
 }
