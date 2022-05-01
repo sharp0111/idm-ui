c9dfa79b683dcef6e5302907f92ec4f7c0658ec7
Sergey Nam <shaqk702@gmail.com>
2022-05-01 12:23:36 -0700

    Add Page Not  Found

---
+++ pages/page-not-found/page-not-found.tsx
@@ -0,0 +1,9 @@
+import  React from 'react'
+import { Typography } from '../../components'
+import { MSG_PAGE_NOT_FOUND } from './messages'
+
+export const PageNotFound = () => {
+  return (
+    <Typography>{MSG_PAGE_NOT_FOUND}</Typography>
+  );
+}
