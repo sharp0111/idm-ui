07e89c63b6f0a2271aadbe1a7d466810a2636d9c
Sergey Nam <shaqk702@gmail.com>
2022-06-12 10:18:22 -0700

    Add editorconfig

--- components/buttongroup/buttongroup.tsx
+++ components/buttongroup/buttongroup.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { ButtonGroup as MuiButtonGroup, ButtonGroupProps as MuiButtonGroupProps } from '@mui/material'
 
-import styles from './buttongroup.module.scss'
-
 type ButtonGroupProps = MuiButtonGroupProps;
 
 export function ButtonGroup(props: ButtonGroupProps) {
   return (
-    <div className={styles.main}>
+    <div>
       <MuiButtonGroup {...props} />
     </div>
   )
