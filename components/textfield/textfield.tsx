56d0c5b972281dd1bb9c40b722e6bb9519962895
Sergey Nam <shaqk702@gmail.com>
2022-07-02 11:47:21 -0700

    Remove className from textfield

--- components/textfield/textfield.tsx
+++ components/textfield/textfield.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import { TextField as MuiTextField, TextFieldProps as MuiTextFieldProps } from '@mui/material'
-import styles from './textfield.module.scss'
 
 export type TextFieldProps = MuiTextFieldProps
 
 export const TextField = (props: TextFieldProps) => (
-  <MuiTextField className={styles.main} {...props} />
+  <MuiTextField {...props} />
 )
