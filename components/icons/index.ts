291d85e551fdd6d8270ac50e878a03d2ca3df5c2
Sergey Nam <shaqk702@gmail.com>
2022-08-22 18:59:56 -0700

    Add Mic Icon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,4 +1,5 @@
 import SearchIcon from '@mui/icons-material/Search'
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import MicIcon from '@mui/icons-material/Mic';
 
-export { SearchIcon, PlayCircleOutlineIcon }
+export { SearchIcon, PlayCircleOutlineIcon, MicIcon }
